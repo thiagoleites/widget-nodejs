ed35bcd16206a606997a30fee3d8e738d21af48d
Thiago Leite <thiagoleites@outlook.com.br>
2022-06-10 22:28:09 -0300

    Fix bug start server

---
+++ README.md
@@ -0,0 +1,16 @@
+# WIDGET
+
+---
+## Aplicação back end NodeJS
+
+------
+
+Servidor back-end, de aplicação para reportar bug, complementação do Widget-React
+
+> Para instalar execute os comandos:
+
+<code>npm install</code>
+
+> Para executar:
+
+<code>npm run dev</code>
---
+++ src/adapters/nodemailer/nodemailer-mail-adapter.ts
@@ -0,0 +1,22 @@
+import nodemailer from 'nodemailer';
+import { MailAdapter, SendMailData } from "../mail-adapter";
+
+const transport = nodemailer.createTransport({
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  auth: {
+    user: "32ea58189a44f9",
+    pass: "0c00a17731a157"
+  }
+});
+
+export class NodeMailerMailAdapter implements MailAdapter {
+  async sendMail({ subject, body }: SendMailData) {
+    await transport.sendMail({
+      from: 'Equipe Feedget <oi@feedget.com>',
+      to: 'Thiago Leite <thiagoleites@gmail.com',
+      subject,
+      html: body,
+    });
+  };
+}
